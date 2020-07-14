8484c79d13f8a215be018eeb75f3a0c94d9dc6f1
AlbertXiao <albert.xiao@grapecity.com>
2020-07-14 17:30:21 +0800

    🦠 chore 删除无用

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    GetWatermarkImageBuffer: require('./GetWatermarkImageBuffer'),
-    text2svg: require('./text2svg'),
-    trueTo256: require('./trueTo256')
-};
